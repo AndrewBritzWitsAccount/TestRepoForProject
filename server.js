355baa65e707e4ed686237cb59bb296ff177042f
Andrew Britz <2305109@students.wits.ac.za>
2024-04-22 07:56:46 +0200

    Attempting to deploy to Azure

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const http = require('http').Server(app);
 
 app.use(express.static('public'));
 
-http.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000; // Use the provided port or default to 3000
+
+http.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
